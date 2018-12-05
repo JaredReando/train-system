1d1fb4fe42017379b998c0e3ec17de653abf2a03
Jared Reando, Paige Williams and Rohan Moore <jared@>
2018-12-05 10:24:49 -0800

    Add train methods. Pass all specs

--- lib/train.rb
+++ lib/train.rb
@@ -1,42 +1,42 @@
 require'pry'
 
 class Train
-  attr_reader(:name, :state, :id)
+  attr_reader(:name, :direction, :id)
 
   def initialize(attributes)
     @name = attributes[:name]
-    @state = attributes[:state]
+    @direction = attributes[:direction]
     @id = attributes[:id]
   end
 
-  def self.all_basic(cities)
-    output_cities = []
-    cities.each() do |city|
-      name = city["name"]
-      state = city["state"]
-      id = city["id"].to_i
-      output_cities.push(City.new({:name => name, :state => state, :id => id}))
+  def self.all_basic(trains)
+    output_trains = []
+    trains.each() do |train|
+      name = train["name"]
+      direction = train["direction"]
+      id = train["id"].to_i
+      output_trains.push(Train.new({:name => name, :direction => direction, :id => id}))
     end
-    output_cities
+    output_trains
   end
 
   def self.all
-    returned_cities = DB.exec("SELECT * FROM cities;")
-    City.all_basic(returned_cities)
+    returned_trains = DB.exec("SELECT * FROM trains;")
+    Train.all_basic(returned_trains)
   end
 
   def save
-    result = DB.exec("INSERT INTO cities (name, state) VALUES ('#{@name}', '#{@state}') RETURNING id;")
+    result = DB.exec("INSERT INTO trains (name, direction) VALUES ('#{@name}', '#{@direction}') RETURNING id;")
     @id = result.first["id"].to_i
   end
 
-  def self.all_by_state(state)
-    returned_cities = DB.exec("SELECT * FROM cities WHERE state = '#{state}'")
-    City.all_basic(returned_cities)
+  def self.all_by_direction(direction)
+    returned_trains = DB.exec("SELECT * FROM trains WHERE direction = '#{direction}'")
+    Train.all_basic(returned_trains)
   end
 
   def ==(other_instance)
-    @name == other_instance.name && @state == other_instance.state
+    @name == other_instance.name && @direction == other_instance.direction
   end
 
 end
---
+++ spec/train_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe(Train) do
+  describe(".all") do
+    it("starts off with no lists") do
+      expect(Train.all()).to(eq([]))
+    end
+  end
+
+  describe(".name") do
+    it("returns its name") do
+      train = Train.new({:name => "Red Line", :direction => "West", :id => nil})
+      expect(train.name()).to(eq("Red Line"))
+    end
+  end
+
+  describe(".id") do
+    it("sets its id when saved") do
+      train = Train.new({:name => "Red Line", :direction => "West", :id => nil})
+      train.save()
+      expect(train.id()).to(be_an_instance_of(Integer))
+    end
+  end
+
+  describe(".save") do
+    it("saves train to the database") do
+      train = Train.new({:name => "Red Line", :direction => "West", :id => nil})
+      train.save()
+      expect(Train.all()).to(eq([train]))
+    end
+  end
+
+  describe("#all_by_direction") do
+    it("sorts based off of direction") do
+      train1 = Train.new({:name => "Red Line", :direction => "West", :id => nil})
+      train1.save()
+      train2 = Train.new({:name => "Green Line", :direction => "East", :id => nil})
+      train2.save()
+      expect(Train.all_by_direction("West")).to(eq([train1]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same if the id and name match") do
+      train1 = Train.new({:name => "Red Line", :direction => "West", :id => nil})
+      train2 = Train.new({:name => "Red Line", :direction => "West", :id => nil})
+      expect(train1).to(eq(train2))
+    end
+  end
+
+end
