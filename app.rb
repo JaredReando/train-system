25c6aeeb0fa4f5139d1a5a2d57ab6b32434bc8fc
Jared Reando, Paige Williams and Rohan Moore <paige@>
2018-12-05 13:33:21 -0800

    Add form for operator to add a train.

--- app.rb
+++ app.rb
@@ -1,68 +1,91 @@
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
-require("./lib/task")
-require("./lib/list")
+require("./lib/train")
+require("./lib/city")
+require("./lib/stop")
 require("pg")
 
-DB = PG.connect({:dbname => "to_do"})
+DB = PG.connect({:dbname => "train_system"})
 
 
 get("/") do
-@lists = List.all
+
 (erb :index)
 end
 
-post("/") do
-  if (params[:list_entry] == "")
-    redirect "/"
-  else
-    list_entry = params[:list_entry].capitalize
-    new_list = List.new(:name => list_entry)
-    new_list.save
-    @lists = List.all
-    redirect "/"
-  end
+get("/rider") do
+
+(erb :index)
 end
 
-get("/tasks/:id") do
-@list_id = params[:id].to_i
-@list = List.find_list(@list_id)
-@tasks = @list.tasks
-
-(erb :tasks)
+get("/operator") do
+  @cities = City.all
+  @stops = Stop.all
+  @trains = Train.all
+(erb :operator)
 end
 
-patch("/tasks/:id") do
-  name = params.fetch("name")
-  @list = List.find_list(params.fetch("id").to_i())
-  @list.update({:name => name})
-  @tasks = @list.tasks
-  erb(:tasks)
-end
 
-delete("/tasks/:id") do
-  @list = List.find_list(params.fetch("id").to_i())
-  @list.delete()
-  redirect "/"
-end
+post("/add_train") do
+  train_name = params.fetch("train_name")
+  train_direction = params.fetch("train_direction")
+  train = Train.new({:name => train_name, :direction => train_direction, :id => nil})
+  train.save()
+  
 
-# post("/tasks/:id") do
-# list_id = params[:id]
-# task_description = params[:task_entry]
-#   if (task_description == "")
-#     redirect "/tasks/#{list_id}"
+redirect ("/operator")
+end
+# post("/") do
+#   if (params[:list_entry] == "")
+#     redirect "/"
 #   else
-#     task = Task.new(:description => task_description, :list_id => list_id)
-#     task.save
-#     redirect "/tasks/#{list_id}"
+#     list_entry = params[:list_entry].capitalize
+#     new_list = List.new(:name => list_entry)
+#     new_list.save
+#     @lists = List.all
+#     redirect "/"
 #   end
 # end
-
-post("/add_task") do
-  list_id = params[:list_entry]
-  task_description = params[:description]
-  task = Task.new(:description => task_description, :list_id => list_id)
-  task.save
-  redirect "/"
-end
+#
+# get("/tasks/:id") do
+# @list_id = params[:id].to_i
+# @list = List.find_list(@list_id)
+# @tasks = @list.tasks
+#
+# (erb :tasks)
+# end
+#
+# patch("/tasks/:id") do
+#   name = params.fetch("name")
+#   @list = List.find_list(params.fetch("id").to_i())
+#   @list.update({:name => name})
+#   @tasks = @list.tasks
+#   erb(:tasks)
+# end
+#
+# delete("/tasks/:id") do
+#   @list = List.find_list(params.fetch("id").to_i())
+#   @list.delete()
+#   redirect "/"
+# end
+#
+# # post("/tasks/:id") do
+# # list_id = params[:id]
+# # task_description = params[:task_entry]
+# #   if (task_description == "")
+# #     redirect "/tasks/#{list_id}"
+# #   else
+# #     task = Task.new(:description => task_description, :list_id => list_id)
+# #     task.save
+# #     redirect "/tasks/#{list_id}"
+# #   end
+# # end
+#
+# post("/add_task") do
+#   list_id = params[:list_entry]
+#   task_description = params[:description]
+#   task = Task.new(:description => task_description, :list_id => list_id)
+#   task.save
+#   redirect "/"
+# end
