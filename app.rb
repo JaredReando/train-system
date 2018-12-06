fcc9fdf8bfef526c2acbc6976f9a53d2ae80002c
Jared Reando, Paige Williams and Rohan Moore <paige@>
2018-12-05 16:04:06 -0800

    Add update feature to train and city views.

--- app.rb
+++ app.rb
@@ -92,56 +92,20 @@ delete("/stops/:id") do
   redirect("/trains/#{train_id}")
 end
 
-# post("/") do
-#   if (params[:list_entry] == "")
-#     redirect "/"
-#   else
-#     list_entry = params[:list_entry].capitalize
-#     new_list = List.new(:name => list_entry)
-#     new_list.save
-#     @lists = List.all
-#     redirect "/"
-#   end
-# end
-#
-# get("/tasks/:id") do
-# @list_id = params[:id].to_i
-# @list = List.find_list(@list_id)
-# @tasks = @list.tasks
-#
-# (erb :tasks)
-# end
-#
-# patch("/tasks/:id") do
-#   name = params.fetch("name")
-#   @list = List.find_list(params.fetch("id").to_i())
-#   @list.update({:name => name})
-#   @tasks = @list.tasks
-#   erb(:tasks)
-# end
-#
-# delete("/tasks/:id") do
-#   @list = List.find_list(params.fetch("id").to_i())
-#   @list.delete()
-#   redirect "/"
-# end
-#
-# # post("/tasks/:id") do
-# # list_id = params[:id]
-# # task_description = params[:task_entry]
-# #   if (task_description == "")
-# #     redirect "/tasks/#{list_id}"
-# #   else
-# #     task = Task.new(:description => task_description, :list_id => list_id)
-# #     task.save
-# #     redirect "/tasks/#{list_id}"
-# #   end
-# # end
-#
-# post("/add_task") do
-#   list_id = params[:list_entry]
-#   task_description = params[:description]
-#   task = Task.new(:description => task_description, :list_id => list_id)
-#   task.save
-#   redirect "/"
-# end
+patch("/trains/:id")do
+  train_id = params[:id].to_i
+  name = params[:train_name]
+  direction = params[:train_direction]
+  train = Train.find_by_id(train_id)
+  train.update({:name => name, :direction => direction})
+  redirect ("/trains/#{train_id}")
+end
+
+patch("/cities/:id")do
+  city_id = params[:id].to_i
+  name = params[:city_name]
+  state = params[:state]
+  city = City.find_by_id(city_id)
+  city.update({:name => name, :state => state})
+  redirect ("/cities/#{city_id}")
+end
